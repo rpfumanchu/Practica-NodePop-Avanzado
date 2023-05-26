6b5cb9ed276a2d88ac4d4ccc656d681b009ac0bc
Rober <rp_rober@hotmail.com>
2023-05-27 01:58:07 +0200

    calculador de hipotecas

---
+++ mortgage-calculator/lib/calculateMonthlyFee.js
@@ -0,0 +1,33 @@
+//DONE Función para calcular la cuota mensual de la hipoteca
+
+function calculateMonthlyFee(
+  capital,
+  rate,
+  term,
+  useEuribor = false,
+  euriborValue = 0,
+) {
+  const capitalFloat = parseFloat(capital);
+
+  //NOTE tasa mensual (dividida por 12)
+  let rateFloat = parseFloat(rate) / 100 / 12;
+  const monthlyTerm = parseInt(term) * 12;
+
+  let share =
+    (capitalFloat * rateFloat) / (1 - Math.pow(1 + rateFloat, -monthlyTerm));
+
+  //NOTE Convertir el valor del Euríbor a decimal
+  if (useEuribor) {
+    const euriborFloat = parseFloat(euriborValue);
+    //NOTE Ajuste mensual basado en el Euríbor (proporcionado como porcentaje)
+    const euriborAdjustment = ((capitalFloat * euriborValue) / 100) * rateFloat;
+
+    //NOTE Sumar el valor del Euríbor a la tasa de interés
+    share += euriborAdjustment;
+  }
+
+  //NOTE Redondear a 2 decimales
+  return share.toFixed(2);
+}
+
+module.exports = calculateMonthlyFee;
---
+++ mortgage-calculator/routes/mortgage.js
@@ -0,0 +1,27 @@
+var express = require("express");
+const calculateMonthlyFee = require("../lib/calculateMonthlyFee");
+var router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.render("calculator");
+});
+
+router.post("/", (req, res, next) => {
+  //NOTE Recuperar los parámetros necesarios para el cálculo
+  const { capital, rate, term, useEuribor, euriborValue } = req.body;
+
+  //NOTE Realizar el cálculo de la cuota mensual
+  const result = calculateMonthlyFee(
+    capital,
+    rate,
+    term,
+    useEuribor,
+    euriborValue,
+  );
+
+  // Enviar el resultado como respuesta
+  //res.locals.result = result;
+  res.render("calculator", { result });
+});
+
+module.exports = router;
